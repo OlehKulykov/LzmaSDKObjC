3116cba4f78fb7b9dc9408c4071d23132249e248
OlehKulykov <nonamedemail@gmail.com>
2016-01-03 00:25:11 +0100

    Update licence dates

--- Pod/Classes/src/LzmaSDKObjCFileDecoder.cpp
+++ Pod/Classes/src/LzmaSDKObjCFileDecoder.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2015 Kulykov Oleh <info@resident.name>
+ *   Copyright (c) 2015 - 2016 Kulykov Oleh <info@resident.name>
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
