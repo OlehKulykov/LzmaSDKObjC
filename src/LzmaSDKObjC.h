a0b93ec2b4e06ce2472587c9f2d636f842c76140
OlehKulykov <info@resident.name>
2016-06-27 15:59:22 +0200

    Next pod version

--- src/LzmaSDKObjC.h
+++ src/LzmaSDKObjC.h
@@ -28,7 +28,10 @@
 /*
  *   Lzma SDK for Objective-C based on extended functionality of the C++ LZMA code
  *
- *   Changes on version 2.0.2 (current):
+ *   Changes on version 2.0.3 (current):
+ *   - Remove redurant string conversion code.
+ *
+ *   Changes on version 2.0.2:
  *   - Fix encode settings count. 
  *
  *   Changes on version 2.0.1:
@@ -123,7 +126,7 @@
 
 #define LZMASDKOBJC_VERSION_MAJOR 2
 #define LZMASDKOBJC_VERSION_MINOR 0
-#define LZMASDKOBJC_VERSION_PATCH 2
+#define LZMASDKOBJC_VERSION_PATCH 3
 
 
 #import "LzmaSDKObjCTypes.h"
