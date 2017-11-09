4ddb5fb2b1c556ca64b0b0bccc20c7294f1368fc
Jen <jen@cautionyourblast.com>
2017-11-09 14:08:38 +0000

    Package.swift manifest file

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let packages = Package(
+    name: "LzmaSDKObjC",
+    products: [
+        .library(
+            name: "LzmaSDKObjC",
+            targets: ["LzmaSDKObjC"]),
+
+    ],
+    targets: [
+        .target(
+            name: "LzmaSDKObjC",
+            path:".",
+            sources: ["src", "lmza"],
+            publicHeadersPath: "src/include")
+    ]
+)
+
+
